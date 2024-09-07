ce094d6b776ffb58117b8885f968be699694f875
Plínio Pvv <gangss2@hotmail.com>
2024-09-07 12:08:43 -0300

    +calculadoraComponent.input->NgModel

--- src/app/calculadora/calculadora.component.spec.ts
+++ src/app/calculadora/calculadora.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -28,6 +30,25 @@ describe('CalculadoraComponent', () => {
     expect(component.result).toEqual(10);
   });
 
+  it('should onChange change the NgModel from inputs', fakeAsync(() => {
+    const de = fixture.debugElement;
+    const value1 = 5;
+    const value2 = 6;
+    const num1field = de.query(By.css('input[name="num1"]')).nativeElement;
+    const num2field = de.query(By.css('input[name="num2"]')).nativeElement;
 
+    num1field.focus();
+    num2field.focus();
+
+    num1field.value = value1;
+    num2field.value = value2;
+    num1field.dispatchEvent(new Event('input'));
+    num2field.dispatchEvent(new Event('input'));
+    tick();
+    fixture.detectChanges();
+
+    expect(component.num1).toEqual(value1);
+    expect(component.num2).toEqual(value2);
+  }));
 
 });
